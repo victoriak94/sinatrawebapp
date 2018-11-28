698d5b58bfe73449f375c5296222598decde0e03
victoriak94 <vkamila6@gmail.com>
2018-11-28 17:41:36 -0500

    Added favourites view. Used hash to iterate through fav links to display both link and text.

--- server.rb
+++ server.rb
@@ -6,10 +6,25 @@ get '/home' do
   erb :index
 end
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to ('/portfolio')
+end
+
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = {"https://crackmagazine.net/": 'Crack Magazine', "https://djbooth.net/": 'DJ Booth'}
+  erb :favourites
 end
